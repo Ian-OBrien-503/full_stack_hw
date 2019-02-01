4a04edf3ad01b18e27b13ea8b548eae4fdf5616f
Your Name <iobrien@pdx.edu>
2019-01-31 17:07:26 -0800

    node #2URLS

--- node/2-url.js
+++ node/2-url.js
@@ -5,6 +5,7 @@ var url = require('url'); // do not change this line
 var querystring = require('querystring'); // do not change this line
 
 var server = http.createServer(function(req, res) {
+
     // home page
     if (req.url === '/') {
         res.writeHead(200, {
@@ -12,8 +13,8 @@ var server = http.createServer(function(req, res) {
         });
         res.write('you have accessed the root');
         res.end();
-
     }
+
     // different route to /test/hello
     else if (req.url === '/test/hello') {
         res.writeHead(200, {
@@ -21,8 +22,30 @@ var server = http.createServer(function(req, res) {
         });
         res.write('you have accessed "hello" within test');
         res.end();
-
     }
+
+    // different route to /test/world
+    else if (req.url === '/test/world') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('you have accessed "world" within test');
+        res.end();
+    }
+
+    // different route to /attributes?hello=world&lorem=ipsum
+    else if (req.url === '/attributes?hello=world&lorem=ipsum') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write('XXXXXXXXXXXXXXXXXXXXXXXXXXX');
+        res.end();
+    }
+
+
+
+
+    // default case
     else {
         res.end();
     }
@@ -38,15 +61,15 @@ server.listen(process.env.PORT||8080);
 // http://localhost:8080/test/world should return 'you have accessed "world" within test' in plain text
 
 // http://localhost:8080/attributes?hello=world&lorem=ipsum should return the following as html (row order might differ)
-//   <!DOCTYPE html>
-//   <html>
-//     <body>
-//       <table border="1">
-//         <tr><td>hello</td><td>world</td></tr>
-//         <tr><td>lorem</td><td>ipsum</td></tr>
-//       </table>
-//     </body>
-//   </html>
+   <!DOCTYPE html>
+   <html>
+    <body>
+       <table border="1">
+        <tr><td>hello</td><td>world</td></tr>
+        <tr><td>lorem</td><td>ipsum</td></tr>
+       </table>
+     </body>
+   </html>
 
 // http://localhost:8080/attributes?first=1&second=2&third=3 should return the following as html (row order might differ)
 //   <!DOCTYPE html>
