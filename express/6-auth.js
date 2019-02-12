43c7171f5949831287dcf652402588e177722bb5
Your Name <iobrien@pdx.edu>
2019-02-11 23:03:46 -0800

    finally done with auth!

--- express/6-auth.js
+++ express/6-auth.js
@@ -2,33 +2,43 @@
 
 var express = require('express'); // do not change this line
 var passport = require('passport'); // do not change this line
-var strategy = require('passport-http').BasicStrategy; // do not change this line
+var strategy = require('passport-http'); // do not change this line
 var server = express()
 
-passport.use(new strategy(
+passport.use(new strategy.BasicStrategy(
   function(userid, password, done) {
-    User.findOne({ username: userid }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.password != password) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
+      if (userid != 'test')
+        return done(null,false);
+      if(password != 'logmein')
+        return done(null,false);
+      return done(null,userid);
+  }));
 
-server.get('/hello',
-  function(req,res){
+  server.use(passport.initialize());
+  server.use(passport.session());
+
+server.get('/hello', function(req,res){
   res.status(200);
   res.type("text/plain");
   res.send('accessible to everyone');
   res.end();
-})
+});
 
-server.get('/*',
-passport.authenticate('basic', { session: false }),
-  function(req, res) {
-
+server.use(passport.authenticate('basic',{session: false}));
+  server.get('/world', function(req, res) {
+  res.status(200);
+  res.type("text/plain");
+  res.write('only accessible when logged in');
+  res.end();
   });
+
+server.use(passport.authenticate('basic',{session: false}));
+server.get('/test', function(req, res) {
+    res.status(200);
+    res.type("text/plain");
+    res.send('only accessible when logged in');
+    res.end();
+    });
 
 server.listen(process.env.PORT||8080);
 // preface: use the passport middleware and only grant the user "test" with the password "logmein" access
